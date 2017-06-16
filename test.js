c55e9e8349b515c3176cdefbcd07d17a5d88196b
Dmitry <df.creative@gmail.com>
2017-06-15 23:16:46 -0400

    Update test

--- test.js
+++ test.js
@@ -2,8 +2,9 @@
 
 const isPath = require('./')
 const assert = require('assert')
+const isWindows = require('is-windows')()
 
-assert.ok(isPath('C:\\xxx'))
+assert.ok(isWindows ? isPath('C:\\xxx') : !isPath('C:\\xxx'))
 assert.ok(isPath('./something'))
 assert.ok(isPath('../something'))
 assert.ok(!isPath('https://github.com/xyz'))
